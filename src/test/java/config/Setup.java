7e967700e954af912da826d3c07a4038820880f3
salmansrabon <salmansrabon@gmail.com>
2025-10-06 22:44:54 +0600

    code ooptimization

--- src/test/java/config/Setup.java
+++ src/test/java/config/Setup.java
@@ -1,14 +1,14 @@
+package config;
+
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 public class Setup {
-    Properties prop;
+    public Properties prop;
     @BeforeTest
     public void setup() throws IOException {
         prop=new Properties();
